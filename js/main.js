465d2209abb36adf364a1ab5ea691ba72442ffbd
Yurii04UA <uyriparaka@gmail.com>
2022-08-05 00:57:43 +0300

    added some js for input

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const searchInput = document.querySelector(".seatch-panel");
+const searchImg = document.querySelector(".search-img");
+
+searchInput.addEventListener("click", () => {
+  document.querySelector(".seatch-panel").classList.add("active-search");
+});
+
+searchImg.addEventListener("click", () => {
+  document.querySelector(".seatch-panel").classList.toggle("active-search");
+  searchInput.value = "";
+});
+
+searchInput.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    document.querySelector(".seatch-panel").classList.remove("active-search");
+    searchInput.value = "";
+  }
+});
