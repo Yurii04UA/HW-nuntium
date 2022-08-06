ae7b47b386aa95d8bb20f53568e7504ac53be176
Yurii04UA <uyriparaka@gmail.com>
2022-08-06 13:45:24 +0300

    fixed errors
    fixed some input errors and added some behavior functionality

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-const searchInput = document.querySelector(".seatch-panel");
+const searchInput = document.querySelector(".search-panel");
 const searchImg = document.querySelector(".search-img");
 
-searchInput.addEventListener("click", () => {
-  document.querySelector(".seatch-panel").classList.add("active-search");
-});
-
+//open/close input
 searchImg.addEventListener("click", () => {
-  document.querySelector(".seatch-panel").classList.toggle("active-search");
+  document.querySelector(".search-panel").classList.toggle("active-search");
   searchInput.value = "";
 });
 
+//close if keyup enter
 searchInput.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    document.querySelector(".seatch-panel").classList.remove("active-search");
+  if (event.code === "Enter" || event.code === "NumpadEnter") {
+    document.querySelector(".search-panel").classList.remove("active-search");
     searchInput.value = "";
   }
 });
 
-
-//need to add close input when i click on window
+// close if clicked on outside
+document.addEventListener("click", (e) => {
+  if (
+    e.target.className != "search-img" &&
+    e.target.className != "search-panel active-search"
+  ) {
+    document.querySelector(".search-panel").classList.remove("active-search");
+    searchInput.value = "";
+  }
+});
