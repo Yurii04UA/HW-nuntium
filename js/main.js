03b9c219063a8828bb5db9385e0c3b509debbf09
Yurii04UA <uyriparaka@gmail.com>
2022-08-09 14:42:00 +0300

    hotfix: fixed the js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 const searchInput = document.querySelector(".search-panel");
 const searchImg = document.querySelector(".search-img");
+const search = document.querySelector('.search')
 
+console.log(searchInput.classList);
 //open/close input
 searchImg.addEventListener("click", () => {
-  document.querySelector(".search-panel").classList.toggle("active-search");
+  searchInput.classList.toggle("active-search");
   searchInput.value = "";
 });
 
 //close if keyup enter
 searchInput.addEventListener("keyup", (event) => {
   if (event.code === "Enter" || event.code === "NumpadEnter") {
-    document.querySelector(".search-panel").classList.remove("active-search");
+    searchInput.classList.remove("active-search");
     searchInput.value = "";
   }
 });
 
 // close if clicked on outside
-document.addEventListener("click", (e) => {
-  if (
-    e.target.className != "search-img" &&
-    e.target.className != "search-panel active-search"
-  ) {
-    document.querySelector(".search-panel").classList.remove("active-search");
-    searchInput.value = "";
-  }
+search.addEventListener('click',(e)=>{
+    e.stopPropagation()
+  })
+document.addEventListener("click", () => {
+  searchInput.classList.remove("active-search");
+  searchInput.value = "";
 });
